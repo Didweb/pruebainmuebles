f28d560fc30514c8a849433fa09516dacb766a41
Didweb <info@did-web.com>
2021-06-05 18:03:00 +0200

    Unit test for create Tour

---
+++ tests/Double/Backoffice/Commercial/Tour/Domain/TourInMemoryRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ApiInmuebles\Tests\Double\Backoffice\Commercial\Tour\Domain;
+
+
+use ApiInmuebles\Backoffice\Commercial\Tour\Domain\Tour;
+use ApiInmuebles\Backoffice\Commercial\Tour\Domain\TourRepository;
+use ApiInmuebles\Backoffice\Commercial\Tour\Domain\ValueObjects\TourId;
+use Doctrine\Common\Collections\ArrayCollection;
+
+final class TourInMemoryRepository
+{
+    public static function empty(): TourRepository
+    {
+        return self::repository([]);
+    }
+
+    private static function repository(array $crmData): TourRepository
+    {
+        return (
+        new class($crmData) implements TourRepository
+        {
+            private ArrayCollection $arrayCollection;
+
+            public function __construct(?array $crmData)
+            {
+                $this->arrayCollection = new ArrayCollection($crmData);
+            }
+
+            public function save(Tour $tour): void
+            {
+                if ($this->arrayCollection->contains($tour)) {
+                    $this->arrayCollection->removeElement($tour);
+                    $this->arrayCollection->add($tour);
+                } else {
+                    $this->arrayCollection->add($tour);
+                }
+            }
+
+
+            public function find(TourId $tourId): ?Tour
+            {
+                $filter = $this->arrayCollection->filter(
+                    function (Tour $tour) use ($tourId) {
+                        return $tour->id()->equals($tourId);
+                    }
+                );
+
+                return $filter->count() ? $filter->first() : null;
+            }
+        }
+        );
+    }
+}
